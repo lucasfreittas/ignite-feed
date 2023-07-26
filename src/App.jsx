c0d8d11b0b4230f53c6ec97305abb17945a4f8ca
Lucas <l.freittas@outlook.com>
2023-07-26 20:26:01 -0300

    Start ignite feed setup

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { Header } from './components/Header/Header';
+import { Post } from './components/Post/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+import './global.css';
+
+
+
+function App() {
+
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar/>
+        </aside>
+        <main>
+          <Post
+            author="Lucas Freitas"
+            content="Lorem ipsum dolor sit amet consectetur adiscipling elit. Assumenda odio aliquam volup obliqua máxima"
+          />
+          <Post
+            author="Diego Fernandes"
+            content="Texto Legal"
+          />
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props){
+    return(
+        <div>
+            <h3>{props.author}</h3>
+            <p>{props.content}</p>
+        </div>
+    )
+}
