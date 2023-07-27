a6c3e6bb47fb975d74b0a14993f93d1fae7a9b19
Lucas <l.freittas@outlook.com>
2023-07-27 20:03:20 -0300

    Post component ok

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,8 +1,39 @@
+import styles from './Post.module.css'
+
 export function Post(props){
     return(
         <div>
-            <h3>{props.author}</h3>
-            <p>{props.content}</p>
+            <article className={styles.post}>
+                <header>
+                    <div className={styles.author}>
+                        <img className={styles.avatar} src="https://github.com/lucasfreittas.png"/>
+                        <div className={styles.authorInfo}>
+                            <strong>Lucas Freitas</strong>
+                            <span>Web Developer</span>
+                        </div>
+                    </div>
+
+                    <time title="27 de Julho às 19:27" dateTime="2023-07-27  19:27">Publicado há 1h</time>
+                </header>
+
+                <div className={styles.content}>
+                    <p>Fala galeraaa</p>
+                    <p>Acabei de subir mais um projeto do meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat</p>
+                    <p><a href="">lucas.design/projetinho</a></p>
+                    <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+               </div>
+
+               <form className={styles.commentForm}>
+                    <strong>Deixe seu feedback</strong>
+                    <textarea
+                        placeholder='Deixe um comentário'
+                    />
+
+                    <footer>
+                        <button type='submit'>Publicar</button>
+                    </footer>
+               </form>
+            </article>
         </div>
     )
 }
